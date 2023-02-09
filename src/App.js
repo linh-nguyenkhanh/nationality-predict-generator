eb00c36134f8f35a016691ab856e9fa9a41c7727
Linh Nguyen <linh.nguyenkhanhib@gmail.com>
2023-02-09 12:06:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex h-screen items-center justify-center bg-hero bg-cover bg-no-repeat ">
+      <div className="container max-w-xl text-center backdrop-sepia-0">
+        <div>
+          <h1 className="text-white font-bold lg:text-5xl md:text-3xl mx-5 sm:text-2xl ">
+            NATIONALITY PREDICT GENERATOR
+          </h1>
+
+          <p className="text-white my-4 lg:text-xl md:text-lg sm:text-sm">
+            Give a name to predict the nationality of a person
+          </p>
+        </div>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter a name..."
+          className="lg:text-2xl md:text-xl sm:text-sm text-left drop-shadow-lg border-8 rounded border-white  px-2 pr-6 w-[70%]"
+        />
+        <div>
+          <button className="outline outline-offset-4 outline-white-500  lg:text-xl md:text-lg sm:text-md text-white py-3 px-5 border-4 rounded-full bg-sky-500 hover:bg-sky-700 hover:text-white my-4">
+            Country?
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        hero: "url('./assets/pexels-photo-335393.jpeg')",
+      },
+    },
+  },
+  plugins: [],
+};
